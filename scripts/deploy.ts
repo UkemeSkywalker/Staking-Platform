691830263f340b3a1fc2748601e3e35740d4a42d
ukemeSkywalker <skywalkershadow9@gmail.com>
2022-09-29 17:40:24 +0100

    JennyCOin Deployed

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,13 @@
 import { ethers } from "hardhat";
 
 async function main() {
+
+  const JennyCoin = await ethers.getContractFactory("JennyCoins");
+  const coinDeploy = await JennyCoin.deploy();
+  const deployed = await coinDeploy.deployed();
+
+  console.log("JennyCOin deployed at:", deployed.address);
+
 }
 
 main().catch((error) => {
